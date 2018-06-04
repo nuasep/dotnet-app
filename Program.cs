b342aeda933baa917e6957112b0afdd2955a0a45
nuasep <nuasep2604@gmail.com>
2018-06-05 01:05:01 +0700

    commit 1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list_quickstart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "Nuasep", "Ana", "Felipe" };
+            
+            {
+                Console.WriteLine();
+                names.Add("Maria");
+                names.Add("Bill");
+                names.Remove("Ana");
+                foreach (var name in names)
+                {
+                    Console.WriteLine($"Hello {name.ToUpper()}!");
+                }
+
+                
+            }
+        }
+    }
+}
